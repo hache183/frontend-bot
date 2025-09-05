69264c918f515fcc64dc9c8ae9da66ac98f7eec6
Marco Epicoco <marcoepicoco2@gmail.com>
2025-09-05 11:32:37 +0200

    final setup

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,8 @@
 module.exports = {
-  plugins: [
-    require('postcss-flexbugs-fixes'),
-    require('postcss-preset-env')({
-      autoprefixer: { flexbox: 'no-2009' },
-      stage: 3,
-    }),
-    require('tailwindcss'),
-    require('autoprefixer'),
-  ],
+  plugins: {
+    'postcss-flexbugs-fixes': {},
+    'postcss-preset-env': { autoprefixer: { flexbox: 'no-2009' }, stage: 3 },
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
